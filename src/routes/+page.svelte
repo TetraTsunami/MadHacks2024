56f09ec398451d299887335e2214fff9dbbad299
TetraTsunami <78718829+TetraTsunami@users.noreply.github.com>
2024-11-09 14:29:14 -0600

    I hate it here

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+  onMount(async () => {
+    eval(await fetch("./GazeCloudAPI.js").then((res) => res.text()));
+  });
+  onMount(() => {
+    setTimeout(() => {
+      console.log("log", GazeCloudAPI);
+      GazeCloudAPI.OnCalibrationComplete = function(){
+        console.log('gaze Calibration Complete')
+      }         
+      GazeCloudAPI.OnCamDenied = function(){ 
+        console.log('camera access denied')  
+      }         
+      GazeCloudAPI.OnResult = console.log;
+      GazeCloudAPI.OnError = function(msg: any){ console.log('err: ' + msg) }
+      GazeCloudAPI.UseClickRecalibration = true;
+    }, 1000);    
+  });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button  type="button" on:click={() => GazeCloudAPI.StartEyeTracking()}>Start</button>
+<button  type="button" on:click={() => GazeCloudAPI.StopEyeTracking()}>Stop</button>
