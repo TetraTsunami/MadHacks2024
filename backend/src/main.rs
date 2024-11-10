90fada2e8096f7032c4e5e8b461a103a064970ac
SizableShrimp <sizableshrimp@sizableshrimp.me>
2024-11-10 01:38:48 -0600

    Add slightly more more to games REST API

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,24 +1,27 @@
 #[macro_use]
 extern crate rocket;
 
-use std::collections::HashMap;
 use rand::prelude::IteratorRandom;
 use rand::prelude::SliceRandom;
 use rand::Rng;
 use rocket::http::Status;
 use rocket::outcome::IntoOutcome;
+use rocket::outcome::Outcome::Success;
 use rocket::request::{FromParam, FromRequest};
 use rocket::serde::json::Json;
 use rocket::tokio::sync::Mutex;
 use rocket::State;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use uuid::Uuid;
 
+#[derive(Clone)]
 pub struct Player {
     pid: Uuid,
     name: String,
 }
 
+#[derive(Clone)]
 pub struct Game {
     code: String,
     players: Vec<Player>,
@@ -51,20 +54,30 @@ const RANDOM_WORDS: [&str; 11] = [
 //     }
 // }
 
-impl<'r> FromRequest<'r> for Game {
-    type Error = ();
-
-    async fn from_request(request: &'r rocket::Request<'_>) -> rocket::request::Outcome<Self, Self::Error> {
-        // Lookup from state Games
-        let code = request.param::<String>(0).unwrap().unwrap();
-
-        request.rocket().state::<Games>()
-            .map(|&games| -> impl Future<Game> { async {
-                games.0.lock().await.get(&code)
-            } })
-            .or_forward(Status::InternalServerError)
-    }
-}
+// #[rocket::async_trait]
+// impl<'r> FromRequest<'r> for Game {
+//     type Error = ();
+//
+//     async fn from_request(
+//         request: &'r rocket::Request<'_>,
+//     ) -> rocket::request::Outcome<Self, Self::Error> {
+//         // Lookup from state Games
+//         let code = request.param::<String>(0).unwrap().unwrap();
+//
+//         println!("biased code: {:?}", code);
+//
+//         request
+//             .rocket()
+//             .state::<Games>()
+//             .unwrap()
+//             .0
+//             .lock()
+//             .await
+//             .get(&code)
+//             .or_error((Status::NotFound, ()))
+//             .map(|game| game.clone())
+//     }
+// }
 
 pub struct Games(Mutex<HashMap<String, Game>>);
 
@@ -78,7 +91,7 @@ struct ResponseGame {
 async fn create_game(name: &str, games: &State<Games>) -> Json<ResponseGame> {
     // Make game code using RANDOM_WORDS plus random number, use 4 random words and 2 random
     // numbers at the end of 2 of the words randomly
-    let mut words = RANDOM_WORDS.choose_multiple(&mut rand::thread_rng(), 4);
+    let words = RANDOM_WORDS.choose_multiple(&mut rand::thread_rng(), 4);
     let rand_indices = (0..4).choose_multiple(&mut rand::thread_rng(), 2);
     let mut code = String::new();
     for (i, word) in words.enumerate() {
@@ -106,14 +119,15 @@ async fn create_game(name: &str, games: &State<Games>) -> Json<ResponseGame> {
     Json(ResponseGame { code, pid })
 }
 
-// #[get("/games/<code>")]
-#[get("/games")]
-fn get_game(/*game: &Game*/) -> Json<ResponseGame> {
-    Json(ResponseGame {
-        // code: game.code,
-        code: "Hello, world!".to_string(),
+#[get("/games/<code>")]
+async fn get_game(code: &str, games: &State<Games>) -> Option<Json<ResponseGame>> {
+    let mut games = games.0.lock().await;
+    let mut game = games.get_mut(code)?;
+
+    Some(Json(ResponseGame {
+        code: game.code.to_string(),
         pid: Uuid::new_v4(),
-    })
+    }))
 }
 
 #[launch]
