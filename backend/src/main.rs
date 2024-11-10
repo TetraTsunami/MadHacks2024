62e8b7578cb87d5bd8d9469d0b41fdaf186e568d
SizableShrimp <sizableshrimp@sizableshrimp.me>
2024-11-10 01:02:14 -0600

    Add slightly more to games REST API

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,12 +2,14 @@
 extern crate rocket;
 
 use std::collections::HashMap;
-
 use rand::prelude::IteratorRandom;
 use rand::prelude::SliceRandom;
 use rand::Rng;
+use rocket::http::Status;
+use rocket::outcome::IntoOutcome;
 use rocket::request::{FromParam, FromRequest};
 use rocket::serde::json::Json;
+use rocket::tokio::sync::Mutex;
 use rocket::State;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -49,9 +51,22 @@ const RANDOM_WORDS: [&str; 11] = [
 //     }
 // }
 
-pub struct Games {
-    games: HashMap<String, Game>,
+impl<'r> FromRequest<'r> for Game {
+    type Error = ();
+
+    async fn from_request(request: &'r rocket::Request<'_>) -> rocket::request::Outcome<Self, Self::Error> {
+        // Lookup from state Games
+        let code = request.param::<String>(0).unwrap().unwrap();
+
+        request.rocket().state::<Games>()
+            .map(|&games| -> impl Future<Game> { async {
+                games.0.lock().await.get(&code)
+            } })
+            .or_forward(Status::InternalServerError)
+    }
 }
+
+pub struct Games(Mutex<HashMap<String, Game>>);
 
 #[derive(Serialize, Deserialize)]
 struct ResponseGame {
@@ -59,13 +74,12 @@ struct ResponseGame {
     pid: Uuid,
 }
 
-#[post("/games")]
-fn create_game(games: &State<Games>) -> Json<ResponseGame> {
+#[post("/games/<name>")]
+async fn create_game(name: &str, games: &State<Games>) -> Json<ResponseGame> {
     // Make game code using RANDOM_WORDS plus random number, use 4 random words and 2 random
     // numbers at the end of 2 of the words randomly
-    let mut rng = rand::thread_rng();
-    let mut words = RANDOM_WORDS.choose_multiple(&mut rng, 4);
-    let rand_indices = (0..4).choose_multiple(&mut rng, 2);
+    let mut words = RANDOM_WORDS.choose_multiple(&mut rand::thread_rng(), 4);
+    let rand_indices = (0..4).choose_multiple(&mut rand::thread_rng(), 2);
     let mut code = String::new();
     for (i, word) in words.enumerate() {
         if i > 0 {
@@ -73,15 +87,21 @@ fn create_game(games: &State<Games>) -> Json<ResponseGame> {
         }
         code.push_str(word);
         if rand_indices.contains(&i) {
-            code.push_str(&rng.gen_range(1..=9).to_string());
+            code.push_str(&rand::thread_rng().gen_range(1..=9).to_string());
         }
     }
 
     let pid = Uuid::new_v4();
+    // TODO: Setup websocket & add to Player struct
     let game = Game {
-        code,
-        pid,
+        code: code.clone(),
+        players: vec![Player {
+            pid,
+            name: name.to_string(),
+        }],
     };
+
+    games.0.lock().await.insert(code.clone(), game);
 
     Json(ResponseGame { code, pid })
 }
@@ -99,8 +119,6 @@ fn get_game(/*game: &Game*/) -> Json<ResponseGame> {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .manage(Games {
-            games: HashMap::new(),
-        })
+        .manage(Games(HashMap::new().into()))
         .mount("/api", routes![create_game, get_game])
 }
