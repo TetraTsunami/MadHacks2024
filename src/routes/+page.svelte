4184d23b672fc21f3449377de7fc80e8883117ab
SizableShrimp <sizableshrimp@sizableshrimp.me>
2024-11-09 20:55:39 -0600

    Remove WebGazer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,5 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import webgazer from 'webgazer';
 
-  onMount(() => {
-    // The mysterious way the webgazer library is bundled means that the webgazer const
-    // (defined in index.mjs) is not accessible from other files in the library.
-    // So we redefine it here using this janky hack.
-    if (typeof window !== 'undefined') window['webgazer'] = webgazer;
-  });
-  webgazer.setGazeListener(function(data, elapsedTime) {
-    if (data == null) {
-      return;
-    }
-    var xprediction = data.x; //these x coordinates are relative to the viewport
-    var yprediction = data.y; //these y coordinates are relative to the viewport
-    console.log(elapsedTime, xprediction, yprediction); //elapsed time is based on time since begin was called
-  });
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -23,5 +7,3 @@
   Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
   documentation
 </p>
-<button type="button" on:click={() => webgazer.begin()}>Start</button>
-<button type="button" on:click={() => webgazer.pause()}>Stop</button>
